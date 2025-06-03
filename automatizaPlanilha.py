2c35d6e1b785e250276098cbea0c9cb0c095c4ab
juliabarbosa <juliabarbosa068@gmail.com>
2025-06-03 16:27:16 -0300

    comentarios com os próximos passos do projeto

--- automatizaPlanilha.py
+++ automatizaPlanilha.py
@@ -3,12 +3,13 @@ import math
 #primeira coluna -> profundidade
 #segunda coluna -> porosidade(decimal) = 0.192
 #terceira coluna -> permeabilidade(mD)
-
+#ao invés de criar uma nova aba, eu posso só criar novas colunas na mesma planilha
+#agora preciso criar novas tabelas para colocar os resultados dos def que eu criei para poder fazer
 
 #classse que faz as operações de cada coluna
 class AutomatizacaoPlanilha:
-    def __init__(self, planilha):
-        df = pd.read_excel(planilha)
+    def __init__(self, caminho):
+        df = pd.read_excel(caminho)
         self.df = df
         self.profundidade = df['Profundidade']
         self.porosidade = df['Porosity (%)']
@@ -16,17 +17,14 @@ class AutomatizacaoPlanilha:
         self.permeabilidade = df['Permeability (mD)']
 
 
-
-
-    def teste(self, teste1):
-        planilha = {
-            'profundidade': 1,
-            'porosidade': 2,
-            'porosidade1': "testando",
-            'permeabilidade': "tesstando",
+    def teste(self):
+        dados = {
+            'profundidade': self.profundidade,
+            'porosidade': self.porosidade,
+            'porosidade1': self.porosidade1,
+            'permeabilidade':  self.permeabilidade,
         }
-        planilha['profundidade'] = teste1
-        print(planilha)
+        print(dados)
     def rqi(self):
         #0,0314 * raiz(Permeabilidade/Porosidade)
         colunaRQI = 0.0314 * (math.sqrt(self.permeabilidade[0]/self.porosidade1[0]))
@@ -48,6 +46,6 @@ class AutomatizacaoPlanilha:
         pass
 
 teste = AutomatizacaoPlanilha('fruta.xlsx')
-teste.teste('oba')
+teste.teste()
 
 
