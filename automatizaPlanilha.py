f18f21db277ed356108ddf260e03770bef3f36ec
juliabarbosa <juliabarbosa068@gmail.com>
2025-06-05 15:48:03 -0300

    adiciona tela do aplicativo

--- automatizaPlanilha.py
+++ automatizaPlanilha.py
@@ -86,17 +86,27 @@ class AutomatizacaoPlanilha:
         dfColunas = pd.DataFrame(colunas)
 
         writer = pd.ExcelWriter('tabela.xlsx', engine='xlsxwriter')
-
         dfColunas.to_excel(writer, sheet_name='Planilha1', index=False)
 
         workbook = writer.book
         worksheet = writer.sheets['Planilha1']
 
+        # Define a centralização das células na horizontal e vertical
+        cell_format = workbook.add_format({'align': 'center', 'valign': 'vcenter'})
+
+        # Aplica o formato nas células da planilha
+        for col_num, value in enumerate(dfColunas.columns.values):
+            worksheet.write(0, col_num, value, cell_format)
+            for row in range(1, len(dfColunas) + 1):
+                worksheet.write(row, col_num, dfColunas.iloc[row - 1, col_num], cell_format)
+
+        # Define largura das colunas
         worksheet.set_column('A:A', 20)
         worksheet.set_column('B:B', 20)
         worksheet.set_column('C:C', 20)
         worksheet.set_column('D:D', 20)
         worksheet.set_column('E:E', 15)
+        worksheet.set_column('F:F', 15)
         worksheet.set_column('G:G', 15)
 
         writer.close()
---
+++ teste3.py
@@ -0,0 +1,3 @@
+texto = 'tabela.xlsc'
+lista = texto.split('.xlsc')
+print(lista)
